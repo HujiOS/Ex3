9d0f1f420657f5a107e454a13d5daa62d8804e4c
Omer <Omer.shacham@mail.huji.ac.il>
2017-05-20 21:28:01 +0300

    123123

--- testush/test.py
+++ testush/test.py
@@ -32,5 +32,3 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 mappedDirs = map(lambda x: os.path.join(dir_path, x),DIRS)
 print('Search %s %s'%(searchString, ' '.join(mappedDirs)))
 print(' '.join(scores))
-
-
